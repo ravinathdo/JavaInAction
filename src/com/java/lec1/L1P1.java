c167d5849abaf0fc61bdb7beeaa18ca7a6c1654c
DEVZONEDO <DEVZONEDO@DESKTOP-F2GOQTF>
2019-12-12 07:45:14 -0800

    init

---
+++ src/com/java/lec1/L1P1.java
@@ -0,0 +1,15 @@
+/*
+ * Read Me: java class, print statement 
+ */
+package com.java.lec1;
+
+/**
+ *
+ * @author DEVZONEDO
+ */
+public class L1P1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Java is fun");
+    }
+}
