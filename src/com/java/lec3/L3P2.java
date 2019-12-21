460ba0adb5abf1722b3453f5cb43e0011a87f09f
DEVZONEDO <DEVZONEDO@DESKTOP-F2GOQTF>
2019-12-21 06:06:10 +0530

    Lecture 3

---
+++ src/com/java/lec3/L3P2.java
@@ -0,0 +1,27 @@
+/*
+ * switch statement
+ */
+package com.java.lec3;
+
+public class L3P2 {
+    public static void main(String[] args) {
+        
+        int i = 10;
+        
+        switch (i) {
+            case 5:
+                System.out.println("i value is 5");
+                break;
+            case 10:
+                System.out.println("i value is 10");
+                break;
+            case 15:
+                System.out.println("i value is 20");
+                break;
+            default:
+                System.out.println("this is default statement");
+        }
+        
+        
+    }
+}
