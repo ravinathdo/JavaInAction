d68cbdf6962f3bd01656377ea97d1d47a022fb02
DEVZONEDO <DEVZONEDO@DESKTOP-F2GOQTF>
2019-12-21 06:03:39 +0530

    Lecture 3

--- src/com/java/lec3/L3P1.java
+++ src/com/java/lec3/L3P1.java
@@ -3,8 +3,11 @@ package com.java.lec3;
 
 public class L3P1 {
     public static void main(String[] args) {
-        // create student object
+        // create student object, working with object and its methods
         Student stu = new Student();
         stu.printStudentName(); // access object methods
+        //use a return value from a method
+        int studentAge = stu.getStudentAge();
+        System.out.println("Student Age found:"+studentAge);
     }
 }
