9616dc636d863e461da99adea74ba24a3c42f2ec
DEVZONEDO <DEVZONEDO@DESKTOP-F2GOQTF>
2019-12-21 06:01:00 +0530

    Lecture 3

--- src/com/java/lec3/L3P1.java
+++ src/com/java/lec3/L3P1.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.java.lec3;
 
-/**
- *
- * @author DEVZONEDO
- */
 public class L3P1 {
     public static void main(String[] args) {
         // create student object
