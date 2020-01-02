25c1f8a9c1dbc5dc22679179b5b8cc66e715ba47
DEVZONEDO <DEVZONEDO@DESKTOP-F2GOQTF>
2020-01-02 20:04:57 +0530

    Lecture 4

---
+++ src/com/java/lec4/DoWhileLoop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.lec4;
+
+/**
+ *
+ * @author DEVZONEDO
+ */
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Hello do while loop "+i);
+            i++;
+        }while(i<10);
+    }
+ 
+}
---
+++ src/com/java/lec4/WhileLoop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.lec4;
+
+/**
+ *
+ * @author DEVZONEDO
+ */
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<10){
+            System.out.println("Hello while loop "+i);
+        i++;
+        }
+    }
+}
